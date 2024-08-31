4ff34cc4254493bb1477f6421d075f0100b90e91
kalaiaadhav <126946909+kalaiaadhav@users.noreply.github.com>
2024-08-31 14:05:07 +0530

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,6 @@ TAG="latest"
 DEV_REPO="kalaiaadhav/dev"
 # Build the Docker image
 docker build -t $IMAGE_NAME:$TAG .
-
 # Tag the image for the dev repository
 docker tag $IMAGE_NAME:$TAG $DEV_REPO:$TAG
 
