00cc9dc28e72aafd2f6067c44063ac97b4178488
kalaiaadhav <126946909+kalaiaadhav@users.noreply.github.com>
2024-09-03 09:05:23 +0530

    Update deploy.sh

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,13 +4,14 @@
 IMAGE_NAME="myapp1"
 TAG="latest"
 echo "AAdhav12@01" | docker login -u kalaiaadhav --password-stdin
+DEV_REPO="kalaiaadhav/dev"
 PROD_REPO="kalaiaadhav/prod"
 SERVER_USER="ubuntu"
-SERVER_IP="ec2-107-22-152-240.compute-1.amazonaws.com"
+SERVER_IP="ec2-54-85-35-74.compute-1.amazonaws.com"
 
 
 # Pull the latest image from the production repository
-ssh $SERVER_USER@$SERVER_IP "docker pull $PROD_REPO:$TAG"
+ssh $SERVER_USER@$SERVER_IP "docker pull $DEV_REPO:$TAG"
 
 
 # Stop and remove the existing container if it exists
@@ -18,5 +19,5 @@ ssh $SERVER_USER@$SERVER_IP "docker stop $IMAGE_NAME || true"
 ssh $SERVER_USER@$SERVER_IP "docker rm $IMAGE_NAME || true"
 
 # Run the new container
-ssh $SERVER_USER@$SERVER_IP "docker run -d --name $IMAGE_NAME -p 80:80 $PROD_REPO:$TAG"
+ssh $SERVER_USER@$SERVER_IP "docker run -d --name $IMAGE_NAME -p 80:80 $DEV_REPO:$TAG"
 
