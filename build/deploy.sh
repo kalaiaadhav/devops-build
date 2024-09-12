03ed4c153a6f2c378fe776da6dcba8de495eaec5
kalaiaadhav <126946909+kalaiaadhav@users.noreply.github.com>
2024-09-12 09:12:28 +0530

    Update deploy.sh

--- build/deploy.sh
+++ build/deploy.sh
@@ -10,12 +10,12 @@ SERVER_USER="ubuntu"
 SERVER_IP="ec2-18-234-208-243.compute-1.amazonaws.com"
 
 # Pull the latest image from the production repository
-ssh -i "C:\Users\Admin\Downloads\newkey01.pem" $SERVER_USER@$SERVER_IP "docker pull $DEV_REPO:$TAG"
+ssh -i "C:/Users/Admin/Downloads/newkey01.pem" $SERVER_USER@$SERVER_IP "docker pull $DEV_REPO:$TAG"
 
 # Stop and remove the existing container if it exists
-ssh -i "C:\Users\Admin\Downloads\newkey01.pem" $SERVER_USER@$SERVER_IP "docker stop $IMAGE_NAME || true"
-ssh -i "C:\Users\Admin\Downloads\newkey01.pem" $SERVER_USER@$SERVER_IP "docker rm $IMAGE_NAME || true"
+ssh -i "C:/Users/Admin/Downloads/newkey01.pem" $SERVER_USER@$SERVER_IP "docker stop $IMAGE_NAME || true"
+ssh -i "C:/Users/Admin/Downloads/newkey01.pem" $SERVER_USER@$SERVER_IP "docker rm $IMAGE_NAME || true"
 
 # Run the new container
-ssh -i "C:\Users\Admin\Downloads\newkey01.pem" $SERVER_USER@$SERVER_IP "docker run -d --name $IMAGE_NAME -p 80:80 $DEV_REPO:$TAG"
+ssh -i "C:/Users/Admin/Downloads/newkey01.pem" $SERVER_USER@$SERVER_IP "docker run -d --name $IMAGE_NAME -p 80:80 $DEV_REPO:$TAG"
 
