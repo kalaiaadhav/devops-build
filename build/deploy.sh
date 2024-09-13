689ff4aa2d6c3e86beb5e0dbffa2b5c32fdeeff1
kalaiaadhav <126946909+kalaiaadhav@users.noreply.github.com>
2024-09-13 23:39:40 +0530

    Update deploy.sh

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 # Set variables
-IMAGE_NAME="myapp_11"
+IMAGE_NAME="myapp_13"
 TAG="latest"
 echo "AAdhav12@01" | docker login -u kalaiaadhav --password-stdin
 DEV_REPO="kalaiaadhav/dev"
 PROD_REPO="kalaiaadhav/prod"
-SERVER_USER="ubuntu"
-SERVER_IP="ec2-18-234-208-243.compute-1.amazonaws.com"
 
 # Pull the latest image from the production repository
-ssh -i "C:/Users/Admin/Downloads/newkey01.pem" $SERVER_USER@$SERVER_IP "docker pull $DEV_REPO:$TAG"
+docker pull $DEV_REPO:$TAG
 
 # Stop and remove the existing container if it exists
-ssh -i "C:/Users/Admin/Downloads/newkey01.pem" $SERVER_USER@$SERVER_IP "docker stop $IMAGE_NAME || true"
-ssh -i "C:/Users/Admin/Downloads/newkey01.pem" $SERVER_USER@$SERVER_IP "docker rm $IMAGE_NAME || true"
+ docker stop $IMAGE_NAME || true
+ docker rm $IMAGE_NAME || true
 
 # Run the new container
-ssh -i "C:/Users/Admin/Downloads/newkey01.pem" $SERVER_USER@$SERVER_IP "docker run -d --name $IMAGE_NAME -p 80:80 $DEV_REPO:$TAG"
+docker run -d --name $IMAGE_NAME -p 80:80 $DEV_REPO:$TAG
 
