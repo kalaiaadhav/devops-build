90da8a67519daec5ae8e890daeba00e0523e1c94
kalaiaadhav <126946909+kalaiaadhav@users.noreply.github.com>
2024-09-13 23:34:02 +0530

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Set variables
-IMAGE_NAME="myapp_11"
+IMAGE_NAME="myapp_13"
 TAG="latest"
 echo "AAdhav12@01" | docker login -u kalaiaadhav --password-stdin
 DEV_REPO="kalaiaadhav/dev"
