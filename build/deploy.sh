be1a72afd44d15c51fb5247e4e4d7ffa8747057d
kalaiaadhav <126946909+kalaiaadhav@users.noreply.github.com>
2024-09-14 00:04:57 +0530

    Update deploy.sh

--- build/deploy.sh
+++ build/deploy.sh
@@ -8,12 +8,14 @@ DEV_REPO="kalaiaadhav/dev"
 PROD_REPO="kalaiaadhav/prod"
 
 # Pull the latest image from the production repository
-docker pull $DEV_REPO:$TAG
+#docker pull $DEV_REPO:$TAG
 
 # Stop and remove the existing container if it exists
- docker stop $IMAGE_NAME || true
- docker rm $IMAGE_NAME || true
+ #docker stop $IMAGE_NAME || true
+ #docker rm $IMAGE_NAME || true
 
 # Run the new container
-docker run -d --name $IMAGE_NAME -p 80:80 $DEV_REPO:$TAG
+#docker run -d --name $IMAGE_NAME -p 80:80 $DEV_REPO:$TAG
 
+ssh -i "C:\Users\Admin\Downloads\newkey01.pem" ubuntu@ec2-44-211-68-140.compute-1.amazonaws.com "docker pull $IMAGE_NAME && docker stop $IMAGE_NAME && docker rm $IMAGE_NAME && docker run -d --name $IMAGE_NAME -p 80:80 $DEV_REPO:$TAG
+"
