1b854acc482d679aa9272904ef0ebaa584b01a41
kalaiaadhav <126946909+kalaiaadhav@users.noreply.github.com>
2024-10-09 08:39:35 +0530

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Set variables
+
 IMAGE_NAME="myapp_17"
 TAG="latest4"
 echo "AAdhav12@01" | docker login -u kalaiaadhav --password-stdin
 DEV_REPO="kalaiaadhav/dev"
 # Build the Docker image
-docker build -t $IMAGE_NAME:$TAG .
+sudo docker build -t $IMAGE_NAME:$TAG .
 # Tag the image for the dev repository
-docker tag $IMAGE_NAME:$TAG $DEV_REPO:$TAG
+sudo docker tag $IMAGE_NAME:$TAG $DEV_REPO:$TAG
 
 # Push the image to the dev repository
-docker push $DEV_REPO:$TAG
+sudo docker push $DEV_REPO:$TAG
